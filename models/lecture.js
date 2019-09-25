e76be500804e80f2f64fd1e704890c2b2913ca11
Criotech <opesiyanbola8991@gmail.com>
2019-09-25 04:15:26 +0100

    lecture section under teacher control

--- models/lecture.js
+++ models/lecture.js
@@ -6,6 +6,7 @@ let lectureSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title: { type: String, required: true },
     lecture: {type: String, required: true},
+    publicid: {type: String, required: true},
     class: { type: mongoose.Schema.Types.ObjectId, ref: 'CLASS', required: true },    
     teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'USER', required: true }        
  });
--- routes/lectures.js
+++ routes/lectures.js
@@ -29,67 +29,158 @@ const storage = cloudinaryStorage({
 });
 
 //file type validation
-function checkFileType(file, cb){
+function checkFileType(file, cb) {
   //Allowed ext
   const filetypes = /pdf|docx|mp4|txt|ppt/;
   // check ext
   const extname = filetypes.test(path.extname
-  (file.originalname).toLowerCase());
+    (file.originalname).toLowerCase());
   //check mime
   const mimetype = filetypes.test(file.mimetype)
 
-  if(mimetype && extname){
+  if (mimetype && extname) {
     return cb(null, true);
-  }else {
+  } else {
     cb(new Error('Error: .mp4, .txt, .docx .ppt and .pdf file types Only!'))
   }
 }
 
 //multer configuration 
 const upload = multer({
-    storage: storage,
-    limits: { 
-        fileSize: 30 * 1024 * 1024,  // 30 MB upload limit
-        files: 1                    // 1 file
-    },
-   fileFilter: function(req, file, cb){
+  storage: storage,
+  limits: {
+    fileSize: 30 * 1024 * 1024,  // 30 MB upload limit
+    files: 1                    // 1 file
+  },
+  fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   }
 }).single("lecture");
 
 
-//teacher post Announcemrnt of a particular class
+//teacher post lecture of a particular class
 router.post('/teacher/:classId', checkAuth, upload, (req, res, next) => {
-     var p = progress()
-    var upload = multer().single('file')
-    
-    console.log(req.body)
-    console.log(req.file)
-    // if (req.userData.userRole === 'teacher') {
-    //      let newLecture = new Lecture({
-    //                 _id: new mongoose.Types.ObjectId(),
-    //                 class: req.params.classId,
-    //                 title: req.body.title,
-    //                 lecture: req.body.content,
-    //                 teacher: req.userData.userId
-    //             })
-    //             newLecture.save()
-    //             .then(result => {
-    //                 res.status(201).json({
-    //                     message: "Announcement uploaded successfully"
-    //                 })
-    //             })
-    //             .catch(err => { 
-    //                 res.status(404).json({
-    //                 message: "Error in uploading announcement",
-    //                 error: err
-    //                 })        
-    //             })
-    // } else {
-    //     res.status(500).json({
-    //         message: 'Access Denied',
-    //     })
-    // }
+  console.log(req.body)
+  console.log(req.file)
+  if (req.userData.userRole === 'teacher') {
+    let newLecture = new Lecture({
+      _id: new mongoose.Types.ObjectId(),
+      class: req.params.classId,
+      title: req.body.title,
+      publicid: req.file.public_id,
+      lecture: req.file.secure_url,
+      teacher: req.userData.userId
+    })
+    newLecture.save()
+      .then(result => {
+        res.status(201).json({
+          message: "Lecture uploaded successfully"
+        })
+      })
+      .catch(err => {
+        res.status(404).json({
+          message: "Error in uploading lecture",
+          error: err
+        })
+      })
+  } else {
+    res.status(500).json({
+      message: 'Access Denied',
+    })
+  }
+})
+
+//teacher gets the list of all lectures
+router.get('/teacher/:classId', checkAuth, (req, res, next) => {
+  Class.findOne({ _id: req.params.classId, teacher: req.userData.userId }).exec()
+    .then(result => {
+      if (result) {
+        Lecture.find().exec()
+          .then(result => {
+            let response = {
+              count: result.length,
+              lecturesData: result.map(doc => {
+                return {
+                  id: doc._id,
+                  lecture: doc.lecture,
+                  publicid: doc.publicid,
+                  request: {
+                    type: "GET",
+                    url: `http://localhost:3000/lectures/teacher/${doc._id}`,
+                  }
+                }
+              })
+            }
+            res.status(200).json(response)
+          })
+          .catch(err => {
+            res.status(404).json({ message: 'error getting data' })
+          })
+      } else {
+        res.status(500).json({ message: 'Access Denied' })
+      }
+    })
+})
+
+//teacher gets the lecture data of a particular class
+router.get('/teacher/:classId/:lectureId', checkAuth, (req, res, next) => {
+  Class.findOne({ _id: req.params.classId, teacher: req.userData.userId }).exec()
+    .then(result => {
+      if (result) {
+        Lecture.findOne({ _id: req.params.lectureId }).exec()
+          .then(result => {
+            res.status(200).json({
+              id: result._id,
+              lecture: result.lecture,
+              publicid: result.publicid
+            })
+          })
+          .catch(err => {
+            res.status(404).json({ message: 'error getting data' })
+          })
+      } else {
+        res.status(500).json({ message: 'Access Denied' })
+      }
+    })
+})
+
+
+//teacher deletes a particular lecture 
+router.delete('/teacher/:classId/:lectureId', checkAuth, (req, res, next) => {
+  if (req.userData.userRole === 'teacher') {
+    Class.findOne({ _id: req.params.classId, teacher: req.userData.userId }).exec()
+      .then(result => {
+        if (result) {
+          Lecture.findOne({ _id: req.params.lectureId }).exec()
+            .then(doc => {
+              let publicId = doc.publicid
+              cloudinary.v2.uploader.destroy(publicId, {invalidate: true, resource_type: "raw"}, function () {
+                  Lecture.remove({ _id: req.params.lectureId })
+                    .exec()
+                    .then(result => {
+                      res.status(201).json({
+                        message: "Lecture has been deleted",
+                        request: {
+                          type: "GET",
+                          url: "http://localhost:3000/lectures/teacher",
+                        }
+                      })
+                    })
+                    .catch(err => {
+                      res.status(404).json({
+                        message: "Error in deleing lecture",
+                        error: err
+                      })
+                    })
+              });
+            })
+        }
+      })
+  } else {
+    res.status(500).json({
+      message: 'Access Denied',
+    })
+  }
 })
 
 module.exports = router;
